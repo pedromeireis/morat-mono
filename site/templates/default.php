5d0b39ef281b1e6770b15ba48abdf6ee129c47e3
Pedro Meireis <hello@meireis.com>
2024-06-12 02:33:53 +0100

    Set styling and elements

--- site/templates/default.php
+++
@@ -1,7 +0,0 @@
-<?php snippet('header') ?>
-
-<main>
-    <h1>Kirby CMS Plainkit | <?= $site->title() ?></h1>
-</main>
-
-<?php snippet('footer') ?>
---
+++ site/templates/home.php
@@ -0,0 +1,7 @@
+<?php snippet('header') ?>
+
+<main class="margin">
+    <h1 class="t-cond">Kirby CMS Plainkit | <?= $site->title() ?></h1>
+</main>
+
+<?php snippet('footer') ?>
